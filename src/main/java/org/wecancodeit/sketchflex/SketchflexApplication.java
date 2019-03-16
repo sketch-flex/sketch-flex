ec96e9b569b84ff544d3aa6afd0e912d6344e685
bryancharleslong <bryancharleslong@gmail.com>
2019-03-16 10:58:58 -0400

    project setup

---
+++ src/main/java/org/wecancodeit/sketchflex/SketchflexApplication.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.sketchflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SketchflexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SketchflexApplication.class, args);
+	}
+
+}
