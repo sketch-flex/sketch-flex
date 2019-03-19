ca6791a41d29a6816b333da47858818e372209f0
ryanandrewmcpherson <ryanmcphersondev@gmail.com>
2019-03-19 15:34:19 -0400

    SketchDeckController & Controller Tests
    Please do not delete this branch as I will still be working on it to
    update the controller with the mappings etc from the mvc tests

---
+++ src/test/java/org/wecancodeit/sketchflex/controllers/SketchDeckControllerTest.java
@@ -0,0 +1,61 @@
+package org.wecancodeit.sketchflex.controllers;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+import org.wecancodeit.sketchflex.models.SketchDeck;
+import org.wecancodeit.sketchflex.repositories.SketchDeckRepository;
+
+
+public class SketchDeckControllerTest {
+	
+	
+	@InjectMocks
+	private SketchDeckController sketchDeckControl;
+	
+	@Mock
+	private SketchDeck sketchDeck1;
+	
+	@Mock
+	private SketchDeck sketchDeck2;
+	
+	@Mock
+	private SketchDeckRepository sketchDeckRepo;
+	
+	@Mock
+	private Model model;
+	
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void shouldAddSingleSketchDeckToModel() throws SketchDeckNotFoundException{
+		long id = 1;
+		when(sketchDeckRepo.findById(id)).thenReturn(Optional.of(sketchDeck1));
+		
+		sketchDeckControl.findOneSketchDeck(id, model);
+		verify(model).addAttribute("single-sketchdeck",sketchDeck1);
+	}
+	
+	@Test
+	public void shouldAddAllSketchDecksToModel() throws Exception{
+		Collection<SketchDeck> allSketchDecks = Arrays.asList(sketchDeck1, sketchDeck2);
+		when(sketchDeckRepo.findAll()).thenReturn(allSketchDecks);
+		
+		sketchDeckControl.findAllSketches(model);
+		verify(model).addAttribute("all-sketchdecks",allSketchDecks);
+	}
+
+}
