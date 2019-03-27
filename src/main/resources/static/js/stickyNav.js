043336d2367969e682ea0f77c4b6dfcdd94362a6
ryanandrewmcpherson <ryanmcphersondev@gmail.com>
2019-03-27 11:59:51 -0400

    rm white label links & linked home page

--- src/main/resources/static/js/stickyNav.js
+++ src/main/resources/static/js/stickyNav.js
@@ -1,7 +1,7 @@
 var navbar = document.getElementById("navbar");
-navbar.innerHTML = `<a class="active" href="http://localhost:8080/upload">Upload</a>
-<a href="http://localhost:8080/sketchdeck">SketchDeck</a>
-<a href="http://localhost:8080/draw">Draw</a>
-<a href="http://localhost:8080/sketches">sketchs</a>
-<a href="http://localhost:8080/sketch">sketch</a>
-<a href="http://localhost:8080/add-sketch">add-sketch</a>`;
+navbar.innerHTML = `
+<a href="http://localhost:8080/home">Home</a>
+<a href="http://localhost:8080/sketchdecks">SketchDecks</a>
+<a href="http://localhost:8080/draw">New Sketch</a>
+<a href="http://localhost:8080/sketches">All Sketches</a>
+<a class="active" href="http://localhost:8080/upload">Upload Sketch</a>`;
