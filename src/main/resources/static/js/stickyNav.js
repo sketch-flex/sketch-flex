8a640bbd2fcb7ee1e07f1a7f65721515f74a3c85
ryanandrewmcpherson <ryanmcphersondev@gmail.com>
2019-03-29 17:17:28 -0400

    many visual updates... check it out!

--- src/main/resources/static/js/stickyNav.js
+++ src/main/resources/static/js/stickyNav.js
@@ -1,7 +1,28 @@
 var navbar = document.getElementById("navbar");
 navbar.innerHTML = `
-<a href="http://localhost:8080/home">Home</a>
-<a href="http://localhost:8080/sketchdecks">SketchDecks</a>
-<a href="http://localhost:8080/draw">New Sketch</a>
-<a href="http://localhost:8080/sketches">All Sketches</a>
-<a class="active" href="http://localhost:8080/upload">Upload Sketch</a>`;
+	<ul id = "myLinks">
+		<li><a href="http://localhost:8080/home">Home</a></li>
+		<li><a href="http://localhost:8080/sketchdecks">SketchDecks</a></li>
+		<li><a href="http://localhost:8080/draw">New Sketch</a></li>
+		<li><a href="http://localhost:8080/sketches">All Sketches</a></li>
+		<li><a class="active" href="http://localhost:8080/upload">Upload Sketch</a></li>
+	</ul>
+	
+	`;
+	
+
+var hamburger = document.getElementById("hambuger");
+
+hamburger = document.addEventListener("click", myFunction);
+	
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+      x.style.height = "0px";
+    } else {
+      x.style.display = "block";
+      x.style.height = "fit-content";
+      
+    }
+  }
