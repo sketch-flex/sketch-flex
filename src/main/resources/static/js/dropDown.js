66ea834dd95ea5d9747766191664b97e96c8cc69
drewgainey <drewgainey@gmail.com>
2019-04-03 20:57:31 -0400

    drop down updates

--- src/main/resources/static/js/dropDown.js
+++ src/main/resources/static/js/dropDown.js
@@ -5,12 +5,12 @@ const dropDown = document.getElementById("dropDown");
 const sketchDeckNameInput = document.getElementById("sketchDeckNameInput");
 
 
+
 // Get All sketchdecks and add to array
 fetch(endpoint)
     .then(blob => blob.json())
     .then(data => sketchDecks.push(...data));
-   
-   
+
 createDropDown();
 
 //create dropdown menu
@@ -23,22 +23,8 @@ function createDropDown() {
 };
 
 //Update Sketch Deck input field based on drop down selection
-dropDown.addEventListener('click',updateInput);
+dropDown.addEventListener('click', updateInput);
 
-function updateInput(){
-    sketchDeckNameInput.setAttribute("value", dropDown.value );
+function updateInput() {
+    sketchDeckNameInput.setAttribute("value", dropDown.value);
 }
-
-//displays list of sketchdecks that match the typing
-// dropDownInput.addEventListener('keyup',displayMatches);
-
-// function findMatches(wordToMatch, sketchDecks) {
-//     return sketchDecks.filter(deck => {
-//         const regex = new RegExp(wordToMatch, 'gi');
-//         return deck.name.match(regex);
-//     });
-// }
-
-// function displayMatches(){
-
-// }
