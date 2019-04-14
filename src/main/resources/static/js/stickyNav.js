a38cd731a7dacca5b0cd8616f9688646c99c0ac9
addiedavis <adeline.davis19@gmail.com>
2019-04-14 11:34:24 -0400

    update stickyNav.js

--- src/main/resources/static/js/stickyNav.js
+++ src/main/resources/static/js/stickyNav.js
@@ -1,8 +1,8 @@
 var ul = document.createElement("ul");
 ul.innerHTML = `
 		<li><a href="http://localhost:8080/home">Home</a></li>
-		<li><a href="http://localhost:8080/sketchdecks">Sketch Decks</a></li>
-		<li><a href="http://localhost:8080/draw">New Sketch</a></li>
+		<li><a href="http://localhost:8080/sketchdecks">SketchDecks</a></li>
+		<li><a href="http://localhost:8080/draw">Draw Sketch</a></li>
 		<li><a href="http://localhost:8080/sketches">All Sketches</a></li>
 		<li><a class="active" href="http://localhost:8080/upload">Upload Sketch</a></li>
 	`;
